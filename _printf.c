bdaeeb3267f4659b329692db0427a5417e7d4aa2
mide1020 <akinrefonayomide1@gmail.com>
2023-05-29 10:22:26 -0700

    printf project

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: printf format.
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -47,16 +47,15 @@ int _printf(const char *format, ...)
 
 	print_buffer(buffer, &buff_ind);
 
-
 	va_end(list);
 
-	return  (printed_chars);
+	return (printed_chars);
 }
 
 /**
- * print_buffer: Prints the contents of the buffer 
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind:The next index to add the lenght char
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
